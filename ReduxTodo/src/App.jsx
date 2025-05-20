9f0b22f94727f403c0b0f42daef4252d63214e1a
unknown <agarwalgorang7@gmail.com>
2025-05-20 15:50:22 +0530

    Redux-react usage

---
+++ ReduxTodo/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/Addtodo'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>TODO using Redux-React</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
